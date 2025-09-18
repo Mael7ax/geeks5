5597b9ec6f87fd36f3fbdb9ba4cfd9494293e01a
Mael <mael7ax@gmail.com>
2025-09-18 12:36:54 +0600

    dz5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Weather} from "./components/weather/Weather.jsx";
+import {Counter} from "./components/Counter/Counter.jsx";
 
 function App() {
 
   return (
     <>
       <Weather/>
+      <Counter/>
     </>
   )
 }
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,19 @@
+import {useCallback, useState} from "react";
+import {ChildButton} from "./ChildButton.jsx";
+
+
+export const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const handleClick = useCallback(() => {
+        setCount(prevCount => prevCount + 1);
+    },[])
+    console.log("Counter render")
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center",gap:"50px", justifyContent: "center"}}>
+            <h3>{count}</h3>
+            <ChildButton onClick={handleClick} label="Click me"/>
+        </div>
+    );
+};
