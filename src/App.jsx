c7a6a2195a184bc697f1fd486853579dd0c9525d
Mael <mael7ax@gmail.com>
2025-09-18 13:09:01 +0600

    dz5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Weather} from "./components/weather/Weather.jsx";
 import {Counter} from "./components/Counter/Counter.jsx";
+import {FilteredList} from "./components/FilterUsers/FilteredList.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Weather/>
       <Counter/>
+        <FilteredList/>
     </>
   )
 }
